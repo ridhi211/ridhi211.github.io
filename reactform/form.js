e18289bace03ede4a70cfa7f9dac0216b71352a6
ridhi211 <kajolsudhakaran5@gmail.com>
2024-08-21 19:14:27 +0530

    react-files

---
+++ reactform/form.js
@@ -0,0 +1,12 @@
+function App(){
+    const [firstName,setFirstName] = useState("")
+    const [lastName,setLastName] = useState("")
+    const [email,setEmail] = useState("")
+    const [contact,setContact] = useState("")
+    const [gender,setGender] = useState("")
+    const [subjects,setSubjects] = useState({
+        maths: true,
+        english: false,
+        physics: false
+    })
+}
